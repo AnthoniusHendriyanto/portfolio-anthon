585098e625efe745d36f347b7a1f202e838c0f7e
AnthoniusHendriyanto <anthonlotus9@gmail.com>
2021-09-27 04:58:14 +0700

    Update NavbarComp.js
    Dah bisa ketengah navbar nya yey

--- src/components/NavbarComp.js
+++ src/components/NavbarComp.js
@@ -9,11 +9,11 @@ export default class NavbarComp extends Component {
           {/* Dalam Container Supaya Rapi ga minggir banget. */}
           <Container>
             {/* Untuk Logo */}
-            <Navbar.Brand href="/">Portfolio Anthon</Navbar.Brand>
+            <Navbar.Brand href="/" className="navbar-brand">Portfolio Anthon</Navbar.Brand>
             {/* Toogle Untuk saat di kecilin. */}
             <Navbar.Toggle />
             <Navbar.Collapse>
-              <Nav className="justify-content-center">
+              <Nav className="offset-md-4">
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#experience">Experience</Nav.Link>
                 <Nav.Link href="#contact">Contact</Nav.Link>
